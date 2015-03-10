2137fb0bdb3dafba0c6637e1f3000ef0c1be5cda
James Kang <jamkang@deloitte.com>
2015-03-10 16:27:01 -0400

    Going to with Angular. Barely starting

--- pnr_quiz/pnr_quiz/settings.py
+++ pnr_quiz/pnr_quiz/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pnr_quiz',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
