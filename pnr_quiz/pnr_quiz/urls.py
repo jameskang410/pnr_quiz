cbaf045e593aea44eed06e57c715e86fc480a30a
James Kang <jameskang410@gmail.com>
2015-03-26 00:39:25 -0400

    Created serializers.py . Need to work on views and urls... and everything

--- pnr_quiz/pnr_quiz/urls.py
+++ pnr_quiz/pnr_quiz/urls.py
@@ -7,8 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    # I actually don't know if pointing to the same view is dangerous haha
-    # Seems like it's okay
+    # Redirects home
     url(r'^$', 'pnr_quiz_site.views.home', name='home'),
     url(r'^home/', 'pnr_quiz_site.views.home', name='home'),
 )
--- pnr_quiz/pnr_quiz_site/serializers.py
+++ pnr_quiz/pnr_quiz_site/serializers.py
@@ -2,3 +2,7 @@ from rest_framework import serializers
 from pnr_quiz_site.models import PnrQuotes
 
 class PnrQuotesSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = PnrQuotes
+		fields = ('person','quote','quotes_key')
